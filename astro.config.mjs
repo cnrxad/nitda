1dc4f055ad4b2f768835a357e3092b5489a7ecde
cnrxad <pabloisyo@outlook.com>
2024-03-31 16:15:22 +0200

    Bug fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import auth from "auth-astro";
-import vercel from "@astrojs/vercel/serverless";
-
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
